0ec85c137ab7cfa5ea2547b0d545f297bd4e60b2
Gregwar <g.passault@gmail.com>
2018-04-24 17:28:22 +0200

    Sessions

---
+++ src/Controller/SessionController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Session;
+use App\Form\SessionType;
+use App\Repository\SessionRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/session")
+ */
+class SessionController extends Controller
+{
+    /**
+     * @Route("/", name="session_index", methods="GET")
+     */
+    public function index(SessionRepository $sessionRepository): Response
+    {
+        return $this->render('session/index.html.twig', ['sessions' => $sessionRepository->findAll()]);
+    }
+
+    /**
+     * @Route("/new", name="session_new", methods="GET|POST")
+     */
+    public function new(Request $request): Response
+    {
+        $session = new Session();
+        $form = $this->createForm(SessionType::class, $session);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($session);
+            $em->flush();
+
+            return $this->redirectToRoute('session_index');
+        }
+
+        return $this->render('session/new.html.twig', [
+            'session' => $session,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="session_show", methods="GET")
+     */
+    public function show(Session $session): Response
+    {
+        return $this->render('session/show.html.twig', ['session' => $session]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="session_edit", methods="GET|POST")
+     */
+    public function edit(Request $request, Session $session): Response
+    {
+        $form = $this->createForm(SessionType::class, $session);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('session_edit', ['id' => $session->getId()]);
+        }
+
+        return $this->render('session/edit.html.twig', [
+            'session' => $session,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="session_delete", methods="DELETE")
+     */
+    public function delete(Request $request, Session $session): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$session->getId(), $request->request->get('_token'))) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($session);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('session_index');
+    }
+}
---
+++ src/Entity/Session.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\SessionRepository")
+ */
+class Session
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $dateCreation;
+
+    public function __construct()
+    {
+        $this->dateCreation = new \DateTime;
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDateCreation(): ?\DateTimeInterface
+    {
+        return $this->dateCreation;
+    }
+
+    public function setDateCreation(\DateTimeInterface $dateCreation): self
+    {
+        $this->dateCreation = $dateCreation;
+
+        return $this;
+    }
+}
